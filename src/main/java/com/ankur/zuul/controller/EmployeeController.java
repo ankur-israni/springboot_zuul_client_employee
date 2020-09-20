c2f5b6aafb9205c4dbff90cdcdb69f7f18ed5814
Ankur Israni <ankur.israni@aa.com>
2020-09-20 17:46:31 -0500

    all done

--- src/main/java/com/ankur/zuul/controller/EmployeeController.java
+++ src/main/java/com/ankur/zuul/controller/EmployeeController.java
@@ -19,7 +19,7 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/services/employee")
-@Api(value = "/services/inventory", tags = ("Employee Management"))
+@Api(value = "/services/employee", tags = ("Employee Management"))
 @CrossOrigin(allowedHeaders = "*", maxAge = 3600)
 public class EmployeeController {
 
@@ -37,16 +37,16 @@ public class EmployeeController {
     }
 
 
-//    @RequestMapping(value = "all", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
-//    @ApiOperation(value = "all", notes = "Fetch all employees", nickname = "all")
-//    @ApiResponses(value = {@ApiResponse(code = 400, message = "Missing / invalid parameter", response = ServiceErrorResponse.class),
-//            @ApiResponse(code = 200, message = "Success", response = EmployeeListAllResponse.class)})
-//    public ResponseEntity<?> all(@RequestHeader(value = CLIENT_ID) String clientId,
-//                                                @Valid @RequestBody EmployeeListAllRequest request) {
-//        System.out.println("Username provided in request: " + request.getUsername());
-//        System.out.println("Password provided in request: " + request.getPassword());
-//            return employeeService.all();
-//    }
+    @RequestMapping(value = "allC", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
+    @ApiOperation(value = "allC", notes = "Fetch all employees", nickname = "all")
+    @ApiResponses(value = {@ApiResponse(code = 400, message = "Missing / invalid parameter", response = ServiceErrorResponse.class),
+            @ApiResponse(code = 200, message = "Success", response = EmployeeListAllResponse.class)})
+    public ResponseEntity<?> allWithCredentials(@RequestHeader(value = CLIENT_ID) String clientId,
+                                                @Valid @RequestBody EmployeeListAllRequest request) {
+        System.out.println("Username provided in request: " + request.getUsername());
+        System.out.println("Password provided in request: " + request.getPassword());
+            return employeeService.all();
+    }
 
     @RequestMapping(value = "all", method = RequestMethod.GET, produces = "application/json")
     @ApiOperation(value = "all", notes = "Fetch all employees", nickname = "all")
@@ -56,17 +56,7 @@ public class EmployeeController {
         return employeeService.all();
     }
 
-    private boolean isValidCredentials(String username, String password) {
-        return StringUtils.isNotEmpty(username) && StringUtils.isNotEmpty(password)
-                && StringUtils.equalsIgnoreCase(VALID_USERNAME, username)
-                && StringUtils.equalsIgnoreCase(VALID_PASSWORD, password);
 
-    }
-
-    private ResponseEntity<ServiceErrorResponse> errorResponse() {
-        ServiceErrorResponse apiError = new ServiceErrorResponse(HttpStatus.UNAUTHORIZED, "9000", "Invalid username or password");
-        return new ResponseEntity<>(apiError, (HttpStatus) apiError.getHttpStatus());
-    }
 
 
 }
